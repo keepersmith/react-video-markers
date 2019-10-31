c6bfb9395211d3250373ea9b0023f98feeb753d4
John Brooks <johnbuckbrooks@gmail.com>
2019-10-31 21:27:49 +0000

    updated to respect new autoplay rules

--- src/index.tsx
+++ src/index.tsx
@@ -49,7 +49,6 @@ function VideoPlayer(props: IProps) {
     onDuration = () => {},
     onMarkerClick = () => {}
   } = props;
-  //var { isPlaying } = props;
 
   useEffect(() => {
     playerEl.current.addEventListener('timeupdate', handleProgress);
@@ -67,8 +66,6 @@ function VideoPlayer(props: IProps) {
           // Autoplay was prevented.
           // Show a "Play" button so that user can start playback.
           onPause();
-          //isPlaying = false;
-          //playerEl.current.pause();
         });
       }
     }
@@ -87,24 +84,16 @@ function VideoPlayer(props: IProps) {
   }, [timeStart]);
 
   useEffect(() => {
-    console.log("RVM 2");
     //isPlaying ? playerEl.current.play() : playerEl.current.pause();
     if (isPlaying) {
       let promise = playerEl.current.play();
       if (promise !== undefined) {
         promise.then(_ => {
-          console.log("GOGO");
           // Autoplay started!
-          //this.handlePlay();
         }).catch(error => {
           // Autoplay was prevented.
-          console.log("NOOOO GOGO");
           // Show a "Play" button so that user can start playback.
           onPause();
-          //isPlaying = false;
-          //playerEl.current.pause();
-          //onPause();
-          //seekToPlayer();
         });
       }
     } else {
